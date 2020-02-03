d5b1ea5ec2d6e8765969d213ff8f61eaed50c8ad
likhita26 <54359054+likhita26@users.noreply.github.com>
2020-02-03 07:42:54 +0530

    Create ridge regression_1.py

---
+++ ridge regression_1.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+df = pd.read_csv('BSLAGRIDX.csv') 
+print(df.shape)
+
+target_column = ['unemploy'] 
+predictors = list(set(list(df.columns))-set(target_column))
+df[predictors] = df[predictors]/df[predictors].max()
+
+X = df[predictors].values
+y = df[target_column].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=40)
+
+rr = Ridge(alpha=0.01)
+rr.fit(X_train, y_train) 
+pred_train_rr= rr.predict(X_train)
+print(np.sqrt(mean_squared_error(y_train,pred_train_rr)))
+print(r2_score(y_train, pred_train_rr))
+
+pred_test_rr= rr.predict(X_test)
+print(np.sqrt(mean_squared_error(y_test,pred_test_rr))) 
+print(r2_score(y_test, pred_test_rr))
